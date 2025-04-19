1f57c06342848c11c21cfe35980f81701f33f1ca
Timileyin <66250695+oloruntolaallbert@users.noreply.github.com>
2025-04-19 22:54:04 +0100

    Add files via upload

--- terraform/connectors.tf
+++ terraform/connectors.tf
@@ -2,6 +2,7 @@
 resource "azurerm_sentinel_data_connector_microsoft_defender_advanced_threat_protection" "defender" {
   name               = "defender-endpoint"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+  depends_on = [azurerm_log_analytics_workspace.law, azurerm_sentinel_log_analytics_workspace_onboarding.sentinel]
 }
 
 # Office 365 Connector
@@ -11,13 +12,54 @@ resource "azurerm_sentinel_data_connector_office_365" "o365" {
   exchange_enabled          = true
   sharepoint_enabled       = true
   teams_enabled            = true
+  depends_on = [azurerm_log_analytics_workspace.law, azurerm_sentinel_log_analytics_workspace_onboarding.sentinel]
 }
 
 # Azure AD (Entra ID) Connector
 resource "azurerm_sentinel_data_connector_azure_active_directory" "aad" {
   name                       = "entra-id"
   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+  depends_on = [azurerm_log_analytics_workspace.law, azurerm_sentinel_log_analytics_workspace_onboarding.sentinel]
 }
 
+# XDR Connector
+resource "azurerm_sentinel_data_connector_microsoft_threat_protection" "xdr" {
+  name                       = "MicrosoftDefenderXDRConnector"
+ log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
+ depends_on = [azurerm_log_analytics_workspace.law, azurerm_sentinel_log_analytics_workspace_onboarding.sentinel]
+}
+
+# Diagnostic settings to send Azure Activity Logs to Log Analytics
+resource "azurerm_monitor_diagnostic_setting" "activity_logs" {
+  name                           = "activity-logs-to-sentinel"
+  target_resource_id             = "/subscriptions/${data.azurerm_subscription.current.subscription_id}"
+  log_analytics_workspace_id     = azurerm_log_analytics_workspace.law.id
+
+  enabled_log {
+    category = "Administrative"
+  }
+  enabled_log {
+    category = "Security"
+  }
+  enabled_log {
+    category = "Policy"
+  }
+  enabled_log {
+    category = "Alert"
+  }
+  enabled_log {
+    category = "Recommendation"
+  }
+  enabled_log {
+    category = "ServiceHealth"
+  }
+  enabled_log {
+    category = "ResourceHealth"
+  }
+  enabled_log {
+    category = "Autoscale"
+  }
+  depends_on = [azurerm_log_analytics_workspace.law, azurerm_sentinel_log_analytics_workspace_onboarding.sentinel]
+}
 # Data provider for current subscription
 data "azurerm_subscription" "current" {}
